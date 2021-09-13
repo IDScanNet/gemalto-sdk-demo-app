e4fea4c8fb5a318a98e766c5b33d2525ee106352
IDSCAN\bmulhall <bmulhall@idscan.net>
2021-09-13 16:45:21 -0500

    checkingin the source and lib directories

---
+++ src/net/idscan/MyErrorHandler.java
@@ -0,0 +1,12 @@
+package net.idscan;
+
+import com.mmm.readers.ErrorCode;
+import com.mmm.readers.ErrorHandler;
+
+public class MyErrorHandler implements ErrorHandler {
+
+    @Override
+    public void OnMMMReaderError(ErrorCode errorCode, String s) {
+        System.out.println("Error in IDScanner: " + errorCode.toString() + ", " + s);
+    }
+}
